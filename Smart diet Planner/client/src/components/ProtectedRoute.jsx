2683f30b28a0e9a23ee5c01461cb7784768ff7ad
rida-irfan <ridairfan2827@gamil.com>
2026-05-15 05:40:08 +0500

    abc

--- Smart diet Planner/client/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { isLoggedIn } from '../services/auth';
-
-const ProtectedRoute = ({ children }) => {
-  return isLoggedIn() ? children : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
